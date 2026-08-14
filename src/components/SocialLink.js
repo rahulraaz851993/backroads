ad7dfbe2a678065620201d4818941cd6f628c446
Rahul Kumar <rahulraaz851993@gmail.com>
2026-08-14 12:45:58 +0530

    fixed nimor issues

--- src/components/SocialLink.js
+++ src/components/SocialLink.js
@@ -1,7 +1,7 @@
 const SocialLink = (props) => {
     return (
         <li>
-            <a href={props.href} target="_blank" className={props.class??'nav-icon'}>
+            <a href={props.href} target="_blank" rel='noreferrer' className={props.class??'nav-icon'}>
                 <i className={props.icon}></i>
             </a>
         </li>
